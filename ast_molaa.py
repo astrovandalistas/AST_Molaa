9e95794436c0c1de70beb9f007a55c5d8390ac69
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-26 22:03:08 -0700

    adds gpio code. needs to be tested

--- ast_molaa.py
+++ ast_molaa.py
@@ -4,7 +4,12 @@ import sys, time, subprocess
 sys.path.append("../LocalNet")
 from interfaces import PrototypeInterface, runPrototype
 
-## TODO: add GPIO stuff
+## import wiringpi lib if available
+HAS_WIRINGPI = True
+try:
+    import wiringpi
+except ImportError:
+    HAS_WIRINGPI = False
 
 class ArmaSonora(PrototypeInterface):
     """ ArmaSonora prototype class
@@ -12,6 +17,29 @@ class ArmaSonora(PrototypeInterface):
         self.messageQ will have all messages coming in from LocalNet """
     (STATE_WAIT, STATE_BANG) = range(2)
     MOTOR_ON_TIME = 20
+    MOTOR_OFF_TIME = 5
+    MOTOR_PINS = [22,23,24]
+
+    def _setupGpio(self):
+        ## setup GPIO object (real or fake)
+        if (HAS_WIRINGPI):
+            self.gpio = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
+            for m in ArmaSonora.MOTOR_PINS:
+                subprocess.call("gpio export "+str(m)+" out 2> /dev/null", shell=True)
+                self.gpio.pinMode(m,self.gpio.OUTPUT)
+                self.gpio.digitalWrite(m,self.gpio.LOW)
+        else:
+            # define fake gpio class with gpio.LOW, gpio.HIGH, gpio.digitalWrite, etc
+            class Gpio(object):
+                def __init__(self):
+                    self.LOW = False
+                    self.HIGH = True
+                    self.OUTPUT = 0
+                def digitalWrite(self,pin=0, val=False):
+                    print "turning fake pin %s to %s" % (pin,val)
+                def pinMode(self,pin=0, val=False):
+                    print "setting fake pin %s to %s" % (pin,val)
+            self.gpio = Gpio()
 
     def setup(self):
         ## pick what to subscribe to
@@ -22,18 +50,26 @@ class ArmaSonora(PrototypeInterface):
         ## some state variables
         self.currentState = ArmaSonora.STATE_WAIT
         self.lastOnTime = time.time()
+        self.lastOffTime = time.time()
+        self._setupGpio()
     def loop(self):
         ## check state
         if ((self.currentState == ArmaSonora.STATE_WAIT) and
+            (time.time()-self.lastOffTime > ArmaSonora.MOTOR_OFF_TIME) and
             (not self.messageQ.empty())):
             (locale,type,txt) = self.messageQ.get()
-            ## TODO: turn gpio(s) on
+            ## turn gpio(s) on
+            for m in ArmaSonora.MOTOR_PINS:
+                self.gpio.digitalWrite(m,self.gpio.HIGH)
             self.currentState = ArmaSonora.STATE_BANG
             self.lastOnTime = time.time()
         elif ((self.currentState == ArmaSonora.STATE_BANG) and
               (time.time()-self.lastOnTime > ArmaSonora.MOTOR_ON_TIME)):
-            ## TODO: turn off gpio(s)
+            ## turn off gpio(s)
+            for m in ArmaSonora.MOTOR_PINS:
+                self.gpio.digitalWrite(m,self.gpio.LOW)
             self.currentState = ArmaSonora.STATE_WAIT
+            self.lastOffTime = time.time()
 
 if __name__=="__main__":
     ## TODO: get ip and ports from command line
